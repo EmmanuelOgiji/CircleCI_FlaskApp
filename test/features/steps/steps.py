25c8bd4f2a277997cedfc1dc9c5f49a739846b61
EmmanuelOgiji <epiusogiji@yahoo.com>
2021-05-25 17:11:37 +0100

    Simple Flask Build and Test: - Add webdriver waits for the tests

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -5,6 +5,9 @@ from assertpy import assert_that
 from behave import *
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 @given(u'I access the app')
@@ -12,12 +15,14 @@ def step_impl(context):
     url = context.config.userdata['url']
     context.driver = webdriver.Chrome()
     context.driver.get(url)
+    WebDriverWait(context.driver, 15).until(ec.presence_of_element_located((By.ID, "testButton")))
 
 
 @when(u'I click the button that says Who built this?')
 def step_impl(context):
     button = context.driver.find_element_by_id("testButton")
     button.click()
+    WebDriverWait(context.driver, 15).until(ec.visibility_of_all_elements_located)
 
 
 @then(u'Emmanuel Pius-Ogiji is rendered')
